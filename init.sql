e7026a97fb76246db707016ff613de14741ad799
MoutazFJ <moutazbusiness@gmail.com>
2026-05-02 20:19:04 +0300

    feat: added local docker compose environment

--- init.sql
+++ init.sql
@@ -1,7 +1,10 @@
+-- Creates both service databases and grants the application user access.
+-- The application user (libuser) is created by MariaDB from the
+-- MYSQL_USER / MYSQL_PASSWORD env vars set in docker-compose.yml.
+
 CREATE DATABASE IF NOT EXISTS userdb;
 CREATE DATABASE IF NOT EXISTS bookdb;
 
-CREATE USER IF NOT EXISTS 'Ahmed'@'%' IDENTIFIED BY 'Aa01117050928';
-GRANT ALL PRIVILEGES ON userdb.* TO 'Ahmed'@'%';
-GRANT ALL PRIVILEGES ON bookdb.* TO 'Ahmed'@'%';
+GRANT ALL PRIVILEGES ON userdb.* TO 'libuser'@'%';
+GRANT ALL PRIVILEGES ON bookdb.* TO 'libuser'@'%';
 FLUSH PRIVILEGES;
